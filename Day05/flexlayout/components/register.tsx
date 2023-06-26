5d06caecb97dd910bfb5d23e717ac255c75d16cc
Shivam Yadav <Shivamy2@mobiquityinc.com>
2023-06-26 17:16:31 +0530

    Refactored register screen

--- Day05/flexlayout/components/register.tsx
+++ Day05/flexlayout/components/register.tsx
@@ -5,10 +5,6 @@ import { styles } from './styles/style'
 const Register = () => {
   return (
     <View style={[styles.container]}>
-    {/* <View style={styles.cardHeader}>
-        <Image source={{uri:"https://img.freepik.com/free-vector/creative-barbecue-logo-template_23-2149017951.jpg?size=626&ext=jpg&ga=GA1.1.1423566931.1687770777&semt=ais"}} height={300} width={250}/>
-        <Text style={styles.brandText}>Brand</Text>
-    </View> */}
     <View style={[styles.innerContainer,{justifyContent:"center"}]}>
         <View style={[styles.box,{height:400}]}>
                 <Text style={styles.titleText}>Register</Text>
