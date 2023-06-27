b5df8295778d51541a9dccb8a1e80957b3c6aac3
Shivam Yadav <Shivamy2@mobiquityinc.com>
2023-06-27 10:55:36 +0530

    Switch added

--- Day05/Animation/App.tsx
+++ Day05/Animation/App.tsx
@@ -1,8 +1,14 @@
-import { View, Text, Button, SafeAreaView, Animated, TouchableOpacity } from 'react-native'
+import { View, Text, Button, SafeAreaView, Animated, TouchableOpacity, Switch } from 'react-native'
 import React, { useRef, useState } from 'react'
 
 const App = () => {
   const value = useState(new Animated.Value(0))[0];
+  const [enable,setEnable] = useState<boolean>(false);
+  const handleSwitchChange = ()=>{
+    setEnable(prev=>!prev)
+    changeColor();
+
+  }
   const changeColor = ()=>{
     // Animated.timing(value,{
     //   toValue:{x:100,y:100},
@@ -11,25 +17,24 @@ const App = () => {
     // }).start()
     Animated.timing(value,{
       toValue:1000,
-      duration:1000,
+      duration:10000,
       useNativeDriver:false
     }).start()
   }
   return (
     <SafeAreaView style={{flex:1}}>
 <Animated.View style={{
-
     flex:1,
+    justifyContent:'center',
+    alignItems:'center',
     backgroundColor:value.interpolate({
-      inputRange:[0,500,1000],
-      outputRange:["green","red","yellow"]
+      inputRange:[0,1000],
+      outputRange:[enable?"red":"green",enable?"green":"red"]
     })
   }}> 
+  <Switch value={enable} onValueChange={handleSwitchChange}/>
 
 </Animated.View>
-<TouchableOpacity onPress={changeColor}>
-    <Text>Click Me</Text>
-</TouchableOpacity>
     </SafeAreaView>
   )
 }
